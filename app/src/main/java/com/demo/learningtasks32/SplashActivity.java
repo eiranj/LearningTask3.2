a3613f77bde1e8c699bcd64fd8cc482c59a17ddc
eiranj <jettsumaria@gmail.com>
2022-10-24 20:04:22 +0800

    LearningTask3.2

---
+++ app/src/main/java/com/demo/learningtasks32/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.demo.learningtasks32;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MediaPlayer appIntro;
+        appIntro = MediaPlayer.create(SplashActivity.this,R.raw.app_intro);
+        appIntro.start();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                appIntro.stop();
+                finish();
+            }
+        }, 5000);
+    }
+}
